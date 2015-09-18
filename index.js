593cfa1be97ee6119e8fd9f31caf7ad3ed4e41be
arnoschn <arnoschn@users.noreply.github.com>
2015-09-18 12:53:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@
    * @return {String}
    */
   function escapeString (str) {
-      return str.replace(/([.+*?=^:${}()[\]|\/])/g, '\\$1')
+      return str.replace(/([*?=^:${}()[\]|\/])/g, '\\$1')
   }
 
   /**
